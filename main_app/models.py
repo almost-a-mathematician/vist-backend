0efc3024e6edb58295f3f350918187a070fb534a
Vikusha Kotikova <almost.a.mathematician@gmail.com>
2024-01-07 19:36:17 +0300

    Add `WishlistManager` class and use it in `Wishlist` model Add conditional serializing of `birth_date` field depending on `is_hidden_bd` Add extender serializers for `User` and `Wishlist` models

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from . managers import WishlistManager
 
 
 class User(models.Model):
@@ -33,10 +34,12 @@ class UserFriend(models.Model):
         return self.status
 
 class Wishlist(models.Model):
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='owner') # тут 
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='wishlists') 
     name = models.CharField(max_length=20, blank=False)
     users = models.ManyToManyField(User)
-    archived_at = models.DateTimeField(null=True)
+    archived_at = models.DateTimeField(null=True, blank=True)
+
+    objects = WishlistManager()
 
     def __str__(self):
         return self.name
