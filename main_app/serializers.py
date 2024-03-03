1b477cc1a7b03ca62467dcbb33e57b80cedb6d52
Vikusha Kotikova <almost.a.mathematician@gmail.com>
2024-03-03 18:00:20 +0300

    add docker files

--- main_app/serializers.py
+++ main_app/serializers.py
@@ -1,20 +1,21 @@
 from rest_framework import serializers
-
-
 from . models import User, Wishlist, Gift
+from django.contrib import auth
+from rest_framework.exceptions import AuthenticationFailed
+from rest_framework_simplejwt.tokens import RefreshToken, TokenError
 
 class FriendSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('username', 'profile_pic', 'birth_date')
+        fields = ('id', 'username', 'profile_pic', 'birth_date')
 
 class UserSerializer(serializers.ModelSerializer):
     friends = FriendSerializer(many=True)
     
     class Meta:
         model = User
-        fields = ('id', 'username', 'profile_pic', 'birth_date', 'friends')
+        fields = ('id', 'username', 'profile_pic', 'birth_date', 'friends', 'is_hidden_bd')
 
     # переопределеяем метод to_representation сериализатора
     def to_representation(self, instance):
@@ -22,15 +23,11 @@ class UserSerializer(serializers.ModelSerializer):
         # это нужно для того чтобы так раз таки выполнить
         # дефолтный механизм приведения модели instance в
         # сериализируемый тип данных data
-        print('lksfhkla', instance)
+       
         data = super().to_representation(instance)
-        print('ljfl;efjl;ewj;', data)
-
+        
         request = self.context['request']
-
-        print('GGGGGGGGG', request)
-
-        viewer_id = int(request.headers['My-User-Id'])
+        viewer_id = request.user.id
 
         if viewer_id != instance.id and instance.is_hidden_bd==True:
             data.pop('birth_date')
@@ -44,21 +41,24 @@ class UserSerializer(serializers.ModelSerializer):
         # пользователя (instance) отключено отображение даты рождения
 
         return data
-   
-class WishlistSerializer(serializers.ModelSerializer):
-    owner = UserSerializer()
-
-    class Meta:
-        model = Wishlist
-        fields = ('id', 'name', 'owner', 'archived_at')
-    
 class GiftSerializer(serializers.ModelSerializer):
-    wishlist = WishlistSerializer()
-    booked_by = UserSerializer()
-    
+    booked_by = UserSerializer(required=False)
+    owner = serializers.    SerializerMethodField() 
     class Meta:
         model = Gift
-        fields = ('id', 'name', 'wishlist', 'img', 'price', 'link_url', 'description', 'booked_by')
+        fields = ('id', 'name', 'wishlist', 'img', 'price', 'link_url', 'description', 'booked_by', 'owner')
+
+    def get_owner(self, obj):
+        owner = obj.wishlist.owner
+        serializer = UserSerializer(owner, context=self.context)
+        return serializer.data
+
+class WishlistSerializer(serializers.ModelSerializer):
+    owner = UserSerializer()
+    gifts = GiftSerializer(many=True)
+    class Meta:
+        model = Wishlist
+        fields = ('id', 'name', 'owner', 'gifts', 'archived_at')
 
 class UserWithWishlistsSerializer(UserSerializer):
     wishlists = WishlistSerializer(many=True) 
@@ -71,3 +71,47 @@ class WishlistWithUsersSerializer(WishlistSerializer):
 
     class Meta(WishlistSerializer.Meta):
         fields = WishlistSerializer.Meta.fields + ('users', )
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(max_length=68, min_length=4, write_only=True)
+    class Meta:
+        model = User
+        fields = ['email', 'username', 'password', 'birth_date', 'profile_pic']
+
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
+    
+class LoginSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(max_length=68, min_length=4,write_only=True)
+    username = serializers.CharField(max_length=255, min_length=3)
+    tokens = serializers.SerializerMethodField()
+    def get_tokens(self, obj):
+        user = User.objects.get(username=obj['username'])
+        return user.tokens()
+    class Meta:
+        model = User
+        fields = ['password','username','tokens']
+    def validate(self, attrs):
+        username = attrs.get('username','')
+        password = attrs.get('password','')
+        user = auth.authenticate(username=username,password=password)
+        if not user:
+            raise AuthenticationFailed('Invalid credentials, try again')
+        if not user.is_active:
+            raise AuthenticationFailed('Account disabled, contact admin')
+        return {
+            'email': user.email,
+            'username': user.username,
+            'tokens': user.tokens
+        }
+
+class LogoutSerializer(serializers.Serializer):
+    refresh = serializers.CharField()
+    def validate(self, attrs):
+        self.token = attrs['refresh']
+        return attrs
+    def save(self):
+        try:
+            RefreshToken(self.token).blacklist()
+        except TokenError:
+            self.fail('bad_token')
--- main_app/views.py
+++ main_app/views.py
@@ -1,29 +1,28 @@
+from rest_framework.exceptions import PermissionDenied, ValidationError
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
-from . serializers import UserSerializer, WishlistSerializer, GiftSerializer, UserWithWishlistsSerializer, WishlistWithUsersSerializer
-from rest_framework import viewsets
+from . serializers import UserSerializer, WishlistSerializer, GiftSerializer, UserWithWishlistsSerializer, WishlistWithUsersSerializer, RegisterSerializer,LoginSerializer,LogoutSerializer
+from rest_framework import viewsets, generics, status, views, permissions
 from rest_framework.response import Response
 from . models import User, Wishlist, Gift
-from rest_framework.decorators import action
+from rest_framework.decorators import action, api_view, parser_classes
 from django.db.models import Q
 from django.db.models import prefetch_related_objects
 from django.db.models import Prefetch
-from . managers import WishlistManager
+from rest_framework.permissions import IsAuthenticated
 
-def index(request):
-
-    return HttpResponse('Hello Vist!')
 
 class UserViewSet(viewsets.ViewSet):
     queryset = User.objects.all()
+    permission_classes = [IsAuthenticated]
 
     def list(self, request):
         serializer = UserSerializer(self.queryset, many=True)
         return Response(serializer.data)
     
     def retrieve(self, request, pk):
-        viewer_id = request.headers["My-User-Id"]
+        viewer_id = request.user.id
 
         user = get_object_or_404(self.queryset, pk=pk)
         
@@ -42,53 +41,275 @@ class UserViewSet(viewsets.ViewSet):
         * попробовать обойтись без функции prefetch_related_objects() в пользу user.wishlists = user.wishlists.filter(...),
         либо просто через user.wishlists.filter(...)
         '''
+        
+    def partial_update(self, request, pk):
+        viewer_id = request.user.id
 
+        if viewer_id != int(pk):
+            raise PermissionDenied()
+ 
+        # находим пользователя по pk
+        user = get_object_or_404(self.queryset, pk=pk) 
 
-class WishlistViewSet(viewsets.ViewSet):
+        serializer = UserWithWishlistsSerializer( 
+            user, 
+            data=request.data, 
+            partial=True,
+            context={'request': request}
+        )
+
+        if not serializer.is_valid():
+            raise ValidationError(serializer.errors)
+        
+        serializer.save()
+        
+        return Response(serializer.data)
     
+class WishlistViewSet(viewsets.ViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = Wishlist.objects.all()
     
     def retrieve(self, request, pk):
-        viewer_id = int(request.headers["My-User-Id"])
+        viewer_id = request.user.id
 
         filtered_queryset = Wishlist.objects.get_visible_for(viewer_id)
 
         wishlist = get_object_or_404(filtered_queryset, pk=pk)
-        
-        serializer_class = WishlistWithUsersSerializer if viewer_id == wishlist.owner.id else WishlistSerializer
+            
+        # sezrializer_class = WishlistWithUsersSerializer if viewer_id == wishlist.owner.id else WishlistSerializer
+
+        serializer_class = self.choose_wishlist_serializer(viewer_id, wishlist)
 
         serializer = serializer_class(wishlist, context={'request': request})
 
         return Response(serializer.data)
+    
+    def choose_wishlist_serializer(self, viewer_id, wishlist):
+           if viewer_id == wishlist.owner.id:
+               return WishlistWithUsersSerializer
+           else:
+               return WishlistSerializer
+
 
     @action(detail=False, methods=['get'])
 
     def archived(self, request):
-        viewer_id = request.headers['My-User-Id']
+        viewer_id = request.user.id
 
         queryset = Wishlist.objects.filter(owner__id=viewer_id, archived_at__isnull=False) # фолс значит информация в поле != null а = дата архивации 
         serializer = WishlistSerializer(queryset, many=True, context={'request': request})
 
         return Response(serializer.data)
+    
+    def partial_update(self, request, pk):
+        viewer_id = request.user.id
 
+        wishlist = get_object_or_404(self.queryset, pk=pk)
+
+        if viewer_id != wishlist.owner.id:
+            raise PermissionDenied()
+
+        serializer = WishlistWithUsersSerializer(
+            wishlist,
+            data=request.data, 
+            partial=True, 
+            context={'request': request}
+        )
+
+        if not serializer.is_valid():
+            raise ValidationError(serializer.errors)
+        
+        serializer.save()
+
+        return Response(serializer.data)
+    
+    @action(detail=True, methods=['put'], url_path='users')
+
+    def update_wishlist_users(self, request, pk):
+        viewer_id = request.user.id
+
+        filtered_queryset = Wishlist.objects.get_visible_for(viewer_id)
+   
+        wishlist = get_object_or_404(filtered_queryset, pk=pk)
+
+        if viewer_id != wishlist.owner.id:
+            raise PermissionDenied()
+
+        request_users = request.data
+        
+
+        if wishlist.owner.id in request_users and len(request_users) > 1:
+            request_users.remove(wishlist.owner.id)
+
+        new_users_list = User.objects.filter(id__in=request_users)
+
+        wishlist.users.set(new_users_list)
+
+        wishlist.save()
+
+        serializer = WishlistWithUsersSerializer(
+            wishlist,
+            context={'request': request}
+        )
+
+        return Response(serializer.data)
+
+    def create(self, request):
+        viewer_id = request.user.id
+        viewer = User.objects.get(id=viewer_id)
+
+        wishlist = Wishlist.objects.create(
+            owner=viewer
+        )
+
+        serializer = WishlistWithUsersSerializer(
+            wishlist,
+            data=request.data, 
+            partial=True, 
+            context={'request': request}
+        )
+
+        if not serializer.is_valid():
+            raise ValidationError(serializer.errors)
+        
+        serializer.save()
+
+        return Response(serializer.data)
 
 class GiftViewSet(viewsets.ViewSet):
     queryset = Gift.objects.all()
-
-    def list(self, request):
-        serializer = GiftSerializer(self.queryset, many=True, context={'request': request})
-        return Response(serializer.data)
+    permission_classes = [IsAuthenticated]
     
-    def retrieve(self, request, pk):
-        gift = get_object_or_404(self.queryset, pk=pk)
-        serializer = GiftSerializer(gift, context={'request': request})
-        return Response(serializer.data)
+    @action(detail=False, methods=['get'], url_path='booked')
 
-    @action(detail=False, methods=['get'])
     def booked(self, request):
-        viewer_id = request.headers['My-User-Id'] 
+        viewer_id = request.user.id 
 
         queryset = Gift.objects.filter(booked_by__id=viewer_id)
+
         serializer = GiftSerializer(queryset, many=True, context={'request': request})
 
         return Response(serializer.data)
+    
+    def partial_update(self, request, pk):
+        viewer_id = request.user.id
+
+        gift = get_object_or_404(self.queryset, pk=pk)
+
+        if viewer_id != gift.wishlist.owner.id:
+            raise PermissionDenied()
+        
+        serializer = GiftSerializer(
+            gift, 
+            data = request.data, 
+            partial=True,
+            context={'request': request}
+        )
+
+        if not serializer.is_valid():
+            raise ValidationError(serializer.error)
+        
+        serializer.save()
+
+        return Response(serializer.data)
+    
+    @action(detail=True, methods=['post'], url_path='booked_by')
+
+    def gift_was_booked_by(self, request, pk):
+        viewer_id = request.user.id
+
+        visible_wishlist = Wishlist.objects.get_visible_for(viewer_id)
+
+        gift = get_object_or_404(self.queryset, pk=pk)
+
+        gift_owner = gift.wishlist.owner
+
+        if not visible_wishlist.filter(gifts__id=pk).exists():
+            raise PermissionDenied
+        
+        if viewer_id != gift_owner.id and gift_owner.are_friends_with(viewer_id) == False:
+            raise PermissionDenied
+
+        if viewer_id != gift_owner.id and gift.booked_by != None and gift.booked_by.id != viewer_id:
+            raise PermissionDenied
+        
+        if request.data == None:
+            gift.booked_by = None
+        else:
+            gift.booked_by = User.objects.get(id=viewer_id)
+
+        gift.save()
+        
+        serializer = GiftSerializer(gift, context={'request': request})
+
+        return Response(serializer.data)
+
+    def create(self, request):
+        viewer_id = request.user.id
+
+        queryset = Wishlist.objects.all()
+
+        wishlist = get_object_or_404(queryset, pk=request.data['wishlist'])
+
+        if wishlist.owner.id != viewer_id:
+            raise PermissionDenied
+            
+        serializer = GiftSerializer(
+            data=request.data,  
+            context={'request': request}
+        )
+
+        if not serializer.is_valid():
+            raise ValidationError(serializer.errors)
+        
+        serializer.save()
+
+        return Response(serializer.data)
+
+    def delete(self, request, pk):
+        viewer_id = request.user.id
+
+        gift = get_object_or_404(self.queryset, pk=pk)
+        
+        if viewer_id == gift.wishlist.owner.id:
+            gift.delete()
+        else:
+            raise PermissionDenied
+        
+        return Response()
+    
+class RegisterView(generics.GenericAPIView):
+    serializer_class = RegisterSerializer
+    
+    def post(self,request):
+        serializer = self.serializer_class(data=request.data)
+
+        serializer.is_valid(raise_exception=True)
+
+        serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+class LoginAPIView(generics.GenericAPIView):
+    serializer_class = LoginSerializer
+
+    def post(self,request):
+        serializer = self.serializer_class(data=request.data)
+
+        serializer.is_valid(raise_exception=True)
+        
+        return Response(serializer.data,status=status.HTTP_200_OK)
+
+class LogoutAPIView(generics.GenericAPIView):
+    serializer_class = LogoutSerializer
+
+    permission_classes = (permissions.IsAuthenticated, )
+    
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+
+        serializer.is_valid(raise_exception=True)
+
+        serializer.save()
+        
+        return Response(status=status.HTTP_204_NO_CONTENT)
